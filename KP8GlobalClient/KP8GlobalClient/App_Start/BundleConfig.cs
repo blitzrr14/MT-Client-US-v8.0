49c8952673c006df022beb8546dc99b73f2fea2d
Ric Ryan Ybanez <blitzrr14@gmail.com>
2017-08-25 14:29:56 -0700

    add sendoutSenderInformation/menubar

--- KP8GlobalClient/KP8GlobalClient/App_Start/BundleConfig.cs
+++ KP8GlobalClient/KP8GlobalClient/App_Start/BundleConfig.cs
@@ -19,6 +19,7 @@ namespace KP8GlobalClient
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/_Layout.js",
+                      "~/Scripts/Sendout.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ KP8GlobalClient/KP8GlobalClient/Models/SendoutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KP8GlobalClient.Models
+{
+    public class SendoutModel
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String middleName { get; set; }
+    }
+}
